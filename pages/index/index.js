c31389d82b9d1e45a5ea42f4eedd98ed80fc9092
dreamyouonly <498752381@qq.com>
2020-09-14 16:16:04 +0800

    000000
    00000

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,6 @@
 //index.js
-//+++++++++++
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
     motto: 'Hello World',
@@ -52,11 +50,50 @@ Page({
       hasUserInfo: true
     })
   },
-  testchange:function(){
-    // this.motto='change successful'
-    this.setData({
-      motto:'修改成功'
+  openButton:function(){
+    console.log("尝试打开LED灯!")
+    var that;
+    var Util = require( 'util.js' );
+    
+    wx.request({
+      url: 'http://59.46.220.242:9090/index/ctrl',
+      complete: (res) => {},
+      data: Util.json2Form( { status: "on" }),
+      dataType: "json",
+      fail: (res) => {},
+      header: { 
+            "Content-Type": "application/x-www-form-urlencoded"
+           },
+      method: "POST",
+      success: function(res){
+        console.log(res)
+        wx.showToast({
+          title: '打开LED成功！',
+        })
+      }
+    })
+
+  },
+  shutdownButton:function(){
+    console.log("尝试关闭LED灯！")
+    var Util = require( 'util.js' );
+    Util.json2Form( { status: "off" })
+    wx.request({
+      url: 'http://59.46.220.242:9090/index/ctrl',
+      complete: (res) => {},
+      data: Util.json2Form( { status: "off" }),
+      dataType: "json",
+      fail: (res) => {},
+      header: { 
+            "Content-Type": "application/x-www-form-urlencoded"
+           },
+      method: "POST",
+      success: function(res){
+        console.log(res.data)
+        wx.showToast({
+          title: '关闭LED成功！',
+        })
+      }
     })
   }
-
 })
